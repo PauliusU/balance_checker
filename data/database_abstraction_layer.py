0a6526baad48b9213469c5321fdfe098ade19f41
PauliusU <48020370+PauliusU@users.noreply.github.com>
2021-01-04 01:01:55 +0200

    Add functions to analyse net profit (loss), total invested amount, current portfolio size

--- data/database_abstraction_layer.py
+++ data/database_abstraction_layer.py
@@ -39,42 +39,80 @@ def create_or_update_balance(balance_amount: float, platform_name: str) -> Balan
     return balance
 
 
-def get_balances_of_today():
+def get_balances_of_today() -> list:
     session = db_connection.create_db_session()
     date_of_today: str = datetime.datetime.today().strftime('%Y-%m-%d')  # E.g. "2021-01-01"
 
-    balances_bondora = session.query(Balance).filter(Balance.date == date_of_today).all()
-    for balance in balances_bondora:
+    balances_of_today = session.query(Balance).filter(Balance.date == date_of_today).all()
+    for balance in balances_of_today:
         print(f"{balance.date} {balance.platform_name} {balance.balance}")
 
     session.close()
 
+    return list(balances_of_today)
 
-def get_total_balance_of_today():
+
+def get_total_balance_of_today() -> float:
     session = db_connection.create_db_session()
     date_of_today: str = datetime.datetime.today().strftime('%Y-%m-%d')  # E.g. "2021-01-01"
+
+    total_amount = session.query(Balance.date, func.sum(Balance.balance).label("daily_balance")) \
+        .filter(Balance.date == date_of_today) \
+        .first()
+    session.close()
+
+    if total_amount[1]:  # if not None
+        total_amount_rounded: float = round(total_amount.daily_balance, 2)
+        print(f"{total_amount.date} TODAY'S TOTAL: {total_amount_rounded}")
+        return total_amount_rounded
+    else:
+        print(f"{date_of_today} TODAY'S TOTAL: 0")
+        return 0
+
+
+def get_last_daily_balance():
+    session = db_connection.create_db_session()
 
     total_amount = session.query(Balance.date, func.sum(Balance.balance).label("daily_balance")) \
         .group_by(Balance.date) \
         .order_by(Balance.date.desc()) \
         .first()
 
-    print(f"{total_amount.date} TOTAL: {round(total_amount.daily_balance, 2)}")
+    print(f"{total_amount.date} LAST DAILY TOTAL: {round(total_amount.daily_balance, 2)}")
 
     session.close()
 
 
-def get_total_balance_by_day():
+def get_total_balances_by_day() -> list:
     session = db_connection.create_db_session()
 
     total_amounts_by_day = session.query(Balance.date, func.sum(Balance.balance).label("daily_balance")) \
         .group_by(Balance.date) \
         .all()
 
-    for row in total_amounts_by_day:
-        print(f"{row.date} {round(row.daily_balance, 2)}")
+    # for row in total_amounts_by_day:
+    #     print(f"{row.date} {round(row.daily_balance, 2)}")
 
     session.close()
+    return total_amounts_by_day
+
+
+def get_portfolio_size() -> float:
+    return get_total_balances_by_day()[-1][1]
+
+
+def get_total_invested() -> float:
+    session = db_connection.create_db_session()
+
+    total_invested_result = session.query(func.sum(Withdrawal.withdrawal)).first()
+    session.close()
+
+    total_invested_float: float = tuple(total_invested_result)[0]
+    return round(total_invested_float, 2)
+
+
+def get_net_profit_loss() -> float:
+    return round(get_total_invested() + get_portfolio_size(), 2)
 
 
 def create_withdrawal(date: str, platform_name: str, withdrawal_amount: float) -> Withdrawal:
