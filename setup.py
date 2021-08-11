4a85c2f01e72d7aa0f95924f1e4b8fa4a10af339
PauliusU <48020370+PauliusU@users.noreply.github.com>
2021-08-11 11:00:32 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+requires = [
+    'beautifulsoup4',
+    'lxml',
+    'python-dotenv',
+    'requests',
+    'selenium',
+    'sqlalchemy'
+]
+
+setup(
+    name='p2p_balance_checker',
+    version='0.1',
+    url='https://github.com/PauliusU/p2p_balance_checker',
+    description='Scrape balance data of European P2P platforms',
+    author='PauliusU',
+    author_email='48020370+PauliusU@users.noreply.github.com',
+    install_requires=requires
+)
